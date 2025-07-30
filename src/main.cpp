181a54dfcdf23ba8d7a4c1340cc0ee7d0aacdbdf
Abdullah449 <ST10450795@vcconnect.edu.za>
2025-07-30 15:26:17 +0200

    tidied up code into methods

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,49 @@ Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 const char *ssid = "TP-Link_B36D";
 const char *password = "73039440";
-const char *moonraker_url = "http://192.168.0.5:4409/printer/objects/query?display_status";
+const char *progressUrl = "http://192.168.0.5:4409/printer/objects/query?display_status";
+const char *statsUrl = "http://192.168.0.5:4409/printer/objects/query?print_stats";
 
 int spinnerIndex = 0;
 unsigned long lastAnimTime = 0;
 unsigned long wifiStartTime = 0;
-const unsigned long wifiTimeout = 150000; // 15 seconds
+const unsigned long wifiTimeout = 150000; // 150 seconds
+
+#pragma region setup
+void setup()
+{
+  Serial.begin(BAUD_RATE); // Connect printer's USB serial to ESP32 RX
+  strip.begin();
+  strip.show();
+  Serial.println("ESP32 Progress Monitor Started");
+
+  connectToWifi();
+
+  Serial.println("\nWiFi connected!");
+}
+
+void connectToWifi()
+{
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi...");
+  wifiStartTime = millis();
+
+  // Spinner while waiting for Wi-Fi
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    showSpinner();
+    Serial.print(".");
+
+    // Timeout after 15s
+    if (millis() - wifiStartTime > wifiTimeout)
+    {
+      Serial.println("\nWiFi Connection Timeout!");
+      showError(); // Red
+      while (true)
+        ; // Stop everything
+    }
+  }
+}
 
 void showSpinner(uint32_t colour = strip.Color(0, 0, 150))
 {
@@ -36,68 +73,64 @@ void showError(uint32_t colour = strip.Color(255, 0, 0))
   strip.show();
 }
 
+#pragma endregion
+
+#pragma region loop
+void loop()
+{
+  // usbserial();
+
+  // wifiserial();
+
+  test();
+}
+
 void test()
 {
   if (WiFi.status() == WL_CONNECTED)
   {
-    HTTPClient http;
-    http.begin(moonraker_url);
+    float progress = getProgress();
+    String state = getState();
 
-    int httpCode = http.GET();
-    String payload = http.getString();
-
-    int progressIndex = payload.indexOf("\"progress\":");
-
-    if (progressIndex != -1)
+    if (state == "printing")
     {
-      float progress = payload.substring(progressIndex + 11).toFloat();
-      String progressString = String(progress * 100);
-
-      Serial.println("Progress: " + progressString + "%");
-
-      int lit_leds = (int)(progress * NUM_LEDS);
-      for (int i = 0; i < NUM_LEDS; i++)
-      {
-        if (i < lit_leds)
-          strip.setPixelColor(i, strip.Color(0, 150, 0)); // Green
-        else
-          strip.setPixelColor(i, 0); // Off
-          strip.show();
-      }
+      showProgress(progress);
     }
-  }
 
-  delay(5000); // Check every 5s
+    delay(5000); // Check every 5s
+  }
 }
 
-void setup()
+void showProgress(float progress)
 {
-  Serial.begin(BAUD_RATE); // Connect printer's USB serial to ESP32 RX
-  strip.begin();
-  strip.show();
-  Serial.println("ESP32 Progress Monitor Started");
+  String progressString = String(progress * 100);
 
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting to WiFi...");
-  wifiStartTime = millis();
-
-  // Spinner while waiting for Wi-Fi
-  while (WiFi.status() != WL_CONNECTED)
+  Serial.println("Progress: " + progressString + "%");
+  int lit_leds = (int)(progress * NUM_LEDS);
+  for (int i = 0; i < NUM_LEDS; i++)
   {
-    showSpinner();
-    Serial.print(".");
-
-    // Timeout after 15s
-    if (millis() - wifiStartTime > wifiTimeout)
-    {
-      Serial.println("\nWiFi Connection Timeout!");
-      showError(); // Red
-      while (true)
-        ; // Stop everything
-    }
+    if (i < lit_leds)
+      strip.setPixelColor(i, strip.Color(0, 150, 0)); // Green
+    else
+      strip.setPixelColor(i, 0); // Off
+    strip.show();
   }
+}
 
-  Serial.println("\nWiFi connected!");
+float getProgress()
+{
+  HTTPClient httpProgress;
+  httpProgress.begin(progressUrl);
+
+  int httpProgress_code = httpProgress.GET();
+  String progressPayload = httpProgress.getString();
+
+  int progressIndex = progressPayload.indexOf("\"progress\":");
+  float progress = progressPayload.substring(progressIndex + 11).toFloat();
+}
+
+String getState(){
+
 }
 
 void usbserial()
@@ -133,87 +166,80 @@ void usbserial()
 
 void wifiserial()
 {
-  //   if (WiFi.status() == WL_CONNECTED)
-  //   {
-  //     HTTPClient http;
-  //     http.begin(moonraker_url);
-  //     int httpCode = http.GET();
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    HTTPClient http;
+    http.begin(progressUrl);
+    int httpCode = http.GET();
 
-  //     if (httpCode == 200)
-  //     {
-  //       String payload = http.getString();
+    if (httpCode == 200)
+    {
+      String payload = http.getString();
 
-  //       int progressIndex = payload.indexOf("\"progress\":");
-  //       int stateIndex = payload.indexOf("\"state\":\"");
+      int progressIndex = payload.indexOf("\"progress\":");
+      int stateIndex = payload.indexOf("\"state\":\"");
 
-  //       if (progressIndex != -1 && stateIndex != -1)
-  //       {
-  //         float progress = payload.substring(progressIndex + 11).toFloat();
-  //         String state = payload.substring(stateIndex + 9, payload.indexOf("\"", stateIndex + 9));
+      if (progressIndex != -1 && stateIndex != -1)
+      {
+        float progress = payload.substring(progressIndex + 11).toFloat();
+        String state = payload.substring(stateIndex + 9, payload.indexOf("\"", stateIndex + 9));
 
-  //         Serial.printf("State: %s, Progress: %.2f\n", state.c_str(), progress);
+        Serial.printf("State: %s, Progress: %.2f\n", state.c_str(), progress);
 
-  //         if (state == "printing")
-  //         {
-  //           int lit_leds = (int)(progress * NUM_LEDS);
-  //           for (int i = 0; i < NUM_LEDS; i++)
-  //           {
-  //             if (i < lit_leds)
-  //               strip.setPixelColor(i, strip.Color(0, 150, 0)); // Green
-  //             else
-  //               strip.setPixelColor(i, 0); // Off
-  //           }
-  //         }
-  //         else if (state == "complete")
-  //         {
-  //           // Blue flash for done
-  //           for (int i = 0; i < NUM_LEDS; i++)
-  //           {
-  //             strip.setPixelColor(i, strip.Color(0, 0, 200));
-  //           }
-  //         }
-  //         else
-  //         {
-  //           // Idle state
-  //           for (int i = 0; i < NUM_LEDS; i++)
-  //           {
-  //             strip.setPixelColor(i, strip.Color(0, 0, 30)); // Dim blue
-  //           }
-  //         }
-  //         strip.show();
-  //       }
-  //     }
-  //     else
-  //     {
-  //       Serial.println("Moonraker API error");
-  //       // Flash yellow
-  //       for (int i = 0; i < NUM_LEDS; i++)
-  //         strip.setPixelColor(i, strip.Color(200, 200, 0));
-  //       strip.show();
-  //       delay(300);
-  //       strip.clear();
-  //       strip.show();
-  //       delay(300);
-  //     }
+        if (state == "printing")
+        {
+          int lit_leds = (int)(progress * NUM_LEDS);
+          for (int i = 0; i < NUM_LEDS; i++)
+          {
+            if (i < lit_leds)
+              strip.setPixelColor(i, strip.Color(0, 150, 0)); // Green
+            else
+              strip.setPixelColor(i, 0); // Off
+          }
+        }
+        else if (state == "complete")
+        {
+          // Blue flash for done
+          for (int i = 0; i < NUM_LEDS; i++)
+          {
+            strip.setPixelColor(i, strip.Color(0, 0, 200));
+          }
+        }
+        else
+        {
+          // Idle state
+          for (int i = 0; i < NUM_LEDS; i++)
+          {
+            strip.setPixelColor(i, strip.Color(0, 0, 30)); // Dim blue
+          }
+        }
+        strip.show();
+      }
+    }
+    else
+    {
+      Serial.println("Moonraker API error");
+      // Flash yellow
+      for (int i = 0; i < NUM_LEDS; i++)
+        strip.setPixelColor(i, strip.Color(200, 200, 0));
+      strip.show();
+      delay(300);
+      strip.clear();
+      strip.show();
+      delay(300);
+    }
 
-  //     http.end();
-  //   }
-  //   else
-  //   {
-  //     Serial.println("Lost Wi-Fi");
-  //     showError(); // Red
-  //     while (true)
-  //       ; // Halt
-  //   }
+    http.end();
+  }
+  else
+  {
+    Serial.println("Lost Wi-Fi");
+    showError(); // Red
+    while (true)
+      ; // Halt
+  }
 
-  //   delay(5000); // Check every 5s
+  delay(5000); // Check every 5s
 }
 
-void loop()
-{
-  // usbserial();
-
-  // wifiserial();
-
-  test();
-}
+#pragma endregion
